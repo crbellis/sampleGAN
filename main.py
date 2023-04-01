2f78eaeb2a5795bd0bc2b2b1c4d731b452e0158d
Chase Bellisime <Bellischase@gmail.com>
2023-03-31 22:56:08 -0700

    demo update

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use("ggplot")
 
-N_MELS = 1024
+N_MELS = 128
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
 # audio 
 audio, sr = librosa.load("./data/909_HatClosed04.wav")
-print(sr)
 
 # Preprocess the input text using the tokenizer
 input_text = "909 hi hat"
@@ -27,9 +26,8 @@ audio_output_layer = tf.keras.layers.Dense(N_MELS*10, activation='linear')(x)
 model = tf.keras.Model(inputs=inputs, outputs=audio_output_layer)
 
 model.compile(loss='mean_squared_error', optimizer='adam')
-print(model.summary())
 
-mel_spec = librosa.feature.melspectrogram(y=audio, sr=sr)
+mel_spec = librosa.feature.melspectrogram(y=audio, sr=sr, n_mels=N_MELS)
 
 audio_features = np.array([mel_spec])
 audio_features = audio_features.reshape(-1, N_MELS*10)
@@ -38,7 +36,5 @@ model.fit(input_data, audio_features, batch_size=1, epochs=30)
 
 predicted_audio = model.predict(np.array([input_ids]))
 predicted_audio = predicted_audio.reshape(-1, N_MELS, 10)
-print(predicted_audio)
-print(audio_features.reshape(-1, N_MELS, 10))
 predicted_audio_waveform = librosa.feature.inverse.mel_to_audio(predicted_audio)
 sf.write(f"predicted_audio_{N_MELS}.wav", predicted_audio_waveform[0], sr, subtype="PCM_24")
